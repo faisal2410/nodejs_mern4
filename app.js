37d465eba7ebca5407763916856c00ed413632ef
Faisal ahmed <faisal2410@yahoo.com>
2023-08-21 20:09:45 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// console.log("Hello from nodejs")
+// console.log("PROCESS: ",process)
+// const http=require('http');
+// const helpers=require("./helpers")
+
+// const server=http.createServer((req,res)=>{
+// res.end("Hello world from nodejs")
+// })
+
+// server.listen(8000)
+
+
+// const total=helpers.sum(10,200);
+
+// console.log("TOTAL: ",total)
+
+const fs=require('fs');
+
+const fileName="target.txt";
+
+// fs.watch(fileName,()=>console.log("File changed"))
+// const data=fs.readFileSync(fileName);
+// console.log(data.toString())
+
+// fs.readFile(fileName,(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(data.toString())
+//     }
+
+// })
+
+
+const errHandler=err=>console.log(err);
+const dataHandler=data=>console.log(data.toString());
+
+fs.readFile(fileName,(err,data)=>{
+    if(err)errHandler(err)
+    dataHandler(data)
+})
+
+
+console.log("Nodejs asynchronous programming ...")
---
+++ note.js
@@ -0,0 +1,17 @@
+
+
+// https://github.com/faisal2410/node-fundamentals
+
+// https://github.com/faisal2410/node-bootcamp
+
+
+// https://v8.dev/
+
+// https://v8.dev/docs
+
+// https://github.com/v8/v8
+
+// https://github.com/nodejs/node
+
+
+// lib, src, v8, uv
